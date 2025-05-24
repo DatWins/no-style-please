bcc6026b7af0199ae84e5284818104e030ac9e9e
DatWins <44272120+DatWins@users.noreply.github.com>
2025-05-24 11:42:04 -0700

    Update mouse_coords.js

--- assets/js/mouse_coords.js
+++ assets/js/mouse_coords.js
@@ -1,6 +1,6 @@
 const p = document.createElement("p");
 p.style.textAlign = "center";
-p.style.fontSize = "25pt";
+p.style.fontSize = "22pt";
 p.innerHTML = "C'mon, move your mouse!"
 document.body.append(p);
 
